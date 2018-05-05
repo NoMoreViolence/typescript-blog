9b36d872f9ab18ee6c5add8eeca9c5e717e6b4b0
NoMoreViolence <ljh86029926@gmail.com>
2018-05-05 20:56:30 +0900

    App.tsx 컴포넌트 정리

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,13 @@
-import * as React from 'react';
-import { Route, Switch } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import * as React from 'react'
+import { Route, Switch } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 // 페이지들
-import Basic from 'components/Pages/Basic';
-import Login from 'components/Pages/Login';
-import Header from 'components/Pages/Header';
-import Admin from 'components/Pages/Admin';
+import Basic from 'components/Pages/Basic'
+import Login from 'components/Pages/Login'
+import Header from 'components/Pages/Header'
+import Admin from 'components/Pages/Admin'
 
 class App extends React.Component {
   // 토큰이 존재할 때 자동 로그인 실현
@@ -23,8 +23,8 @@ class App extends React.Component {
           <Route exact={true} path="/admin" component={Admin} />
         </Switch>
       </React.Fragment>
-    );
+    )
   }
 }
 
-export default App;
+export default App
