12eefc08166e93d38c4f8ff9b7e8cbc4cf515e07
NoMoreViolence <ljh86029926@gmail.com>
2018-05-13 02:05:38 +0900

    서버 기본 설정 변경

---
+++ Server/.eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: 'airbnb-base',
+  rules: {
+    'no-unused-vars': 1,
+    'comma-dangle': 0,
+    'eol-last': 0,
+    'no-console': 0,
+    semi: [2, 'never'],
+    'func-names': 0,
+    'prefer-destructuring': 0,
+    'arrow-parens': 0
+  }
+}
