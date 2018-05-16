d9feda2efa884c1a0fbbd16398c630682b26755c
NoMoreViolence <ljh86029926@gmail.com>
2018-05-17 08:19:55 +0900

    Index.tsx

--- Client/src/index.tsx
+++ Client/src/index.tsx
@@ -1,17 +1,19 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import MainComponent from 'components/MainComponent';
-import registerServiceWorker from './registerServiceWorker';
-import './index.css';
-import store from './store';
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import MainComponent from 'components/MainComponent'
+import registerServiceWorker from './registerServiceWorker'
+import './index.css'
+import store from './store'
 // 리액트 스트랩 CSS 적용
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+// Quill
+import 'react-quill/dist/quill.snow.css' // ES6
 
 ReactDOM.render(
   <Provider store={store}>
     <MainComponent />
   </Provider>,
   document.getElementById('root') as HTMLElement
-);
-registerServiceWorker();
+)
+registerServiceWorker()
