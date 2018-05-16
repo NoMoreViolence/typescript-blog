2642482ac793742a39f1f2ff86d0338a3373a674
NoMoreViolence <ljh86029926@gmail.com>
2018-05-16 09:41:51 +0900

    카테고리 변경 axios 변경

--- Client/src/store/modules/Category.ts
+++ Client/src/store/modules/Category.ts
@@ -36,8 +36,8 @@ function patchCategoryChangeAPI(oldCategory: string, newCategory: string) {
 }
 
 // 카테고리를 삭제하는 API
-function deleteCategoryDeleteAPI(Category: string) {
-  return axios.delete(`/api/${Category}`, {
+function deleteCategoryDeleteAPI(Category: string, doubleCheck: string) {
+  return axios.delete(`/api/${Category}?doubleCheck=${doubleCheck}`, {
     headers: {
       'Content-Type': 'application/json',
       'x-access-token': sessionStorage.getItem('token')
@@ -74,6 +74,7 @@ const DELETE_DELETE_CATEGORY_FAILURE = 'DELETE_DELETE_CATEGORY_FAILURE'
 const CATEGORY_DONE = 'CATEGORY_DONE'
 
 // 카테고리 페이로드 정의 근데 내 실력이 부족해서 명확하게 쓸 부분만 정의할 수가 없었다, 그래서 any값을 우겨넣었다
+// 이제 페이로드 값에 대한 것들을 정의할 수 있게 되었다
 type CategoryPayload = any
 type CategoryInputPayload = string
 
@@ -82,52 +83,72 @@ export const CategoryActions = {
   // 카테고리 로딩하는 API
   getCategory: createAction(GET_BRING_CATEGORYS, getCategorysAPI),
 
-  // 카테고리 추가
-  addCategoryInputChange: createAction<CategoryInputPayload>(POST_ADD_CATEGORY_INPUT_CHANGE),
+  // category Add
+  // <payload, parameter>
+  addCategoryInputChange: createAction<CategoryInputPayload, CategoryInputPayload>(
+    POST_ADD_CATEGORY_INPUT_CHANGE,
+    // parameter,             payload
+    (text: string): string => text
+  ),
   addCategory: createAction(POST_ADD_CATEGORY, postCategoryAddAPI),
+  // category Add
 
-  // 카테고리 변경
-  changeCategoryInputChange: createAction<CategoryInputPayload>(PATCH_CHANGE_CATEGORY_INPUT_CHANGE),
-  changeCategoryCategoryChange: createAction<CategoryInputPayload>(PATCH_CHANGE_CATEGORY_CATEGORY_CHANGE),
+  // category Change
+  changeCategoryInputChange: createAction<CategoryInputPayload, CategoryInputPayload>(
+    PATCH_CHANGE_CATEGORY_INPUT_CHANGE,
+    (text: string): string => text
+  ),
+  changeCategoryCategoryChange: createAction<CategoryInputPayload, CategoryInputPayload>(
+    PATCH_CHANGE_CATEGORY_CATEGORY_CHANGE,
+    (value: string): string => value
+  ),
   changeCategory: createAction(PATCH_CHANGE_CATEGORY, patchCategoryChangeAPI),
+  // category Change
 
-  // 카테고리 삭제
-  deleteCategoryInputChange: createAction<CategoryInputPayload>(DELETE_DELETE_CATEGORY_INPUT_CHANGE),
-  deleteCategoryCategoryChange: createAction<CategoryInputPayload>(DELETE_DELETE_CATEGORY_CATEGORY_CHANGE),
+  // category Delete
+  deleteCategoryInputChange: createAction<CategoryInputPayload, CategoryInputPayload>(
+    DELETE_DELETE_CATEGORY_INPUT_CHANGE,
+    (text: string): string => text
+  ),
+  deleteCategoryCategoryChange: createAction<CategoryInputPayload, CategoryInputPayload>(
+    DELETE_DELETE_CATEGORY_CATEGORY_CHANGE,
+    (value: string): string => value
+  ),
   deleteCategory: createAction(DELETE_DELETE_CATEGORY, deleteCategoryDeleteAPI),
+  // category Delete
 
-  // 모든 작업이 끝났을 때 실행해주는 초기화 변수
+  // EveryThing is Gone
   categoryDone: createAction(CATEGORY_DONE)
 }
-// 카테고리 들어있는 부분의 State 정의
+// Category in Category State
 export interface CategoryStateInside {
-  posts: [string]
+  posts: object[]
   _id: string
   category: string
   __v: number
 }
-// 카테고리 기본 State 타입 정의
+// Category State
 export interface CategoryState {
   categoryPending: boolean
   categoryError: boolean
-  categoryCategory: [CategoryStateInside]
+  categoryCategory: CategoryStateInside[]
 
   addCategoryPending: boolean
-  addCategoryInputValue: string | undefined
+  addCategoryInputValue?: string
 
   changeCategoryPending: boolean
-  changeCategoryCategoryValue: string | undefined
-  changeCategoryInputValue: string | undefined
+  changeCategoryCategoryValue?: string
+  changeCategoryInputValue?: string
 
   deleteCategoryPending: boolean
-  deleteCategoryCategoryValue: string | undefined
-  deleteCategoryInputValue: string | undefined
+  deleteCategoryCategoryValue?: string
+  deleteCategoryInputValue?: string
 }
-// 초기 상태
+// origin state
 const initialState: CategoryState = {
   categoryPending: false,
   categoryError: false,
-  categoryCategory: [{ posts: [''], _id: '', category: '', __v: 0 }],
+  categoryCategory: [],
 
   addCategoryPending: false,
   addCategoryInputValue: '',
@@ -140,6 +161,15 @@ const initialState: CategoryState = {
   deleteCategoryCategoryValue: '삭제할 카테고리 선택',
   deleteCategoryInputValue: ''
 }
+
+// Action: <CategoryPayload> 값을 직접 정해주다가 함수의 리턴타입으로 정의해서 action에 넣어준다
+type AddCategoryInputPayloadAction = ReturnType<typeof CategoryActions.addCategoryInputChange>
+
+type ChangeCategoryInputPayloadAction = ReturnType<typeof CategoryActions.changeCategoryInputChange>
+type ChangeCategoryCategoryPayloadAction = ReturnType<typeof CategoryActions.changeCategoryCategoryChange>
+
+type DeleteCategoryInputPayloadAction = ReturnType<typeof CategoryActions.deleteCategoryInputChange>
+type DeleteCategoryCategoryPayloadAction = ReturnType<typeof CategoryActions.deleteCategoryCategoryChange>
 
 // 액션 상태
 export default handleActions(
@@ -155,18 +185,18 @@ export default handleActions(
       produce(state, (draft: CategoryState) => {
         draft.categoryPending = false
         draft.categoryError = false
-        draft.categoryCategory = action.payload.data.category
+        draft.categoryCategory = action.payload.data.value
       }),
     // 카테고리 가져오기 실패
     [GET_BRING_CATEGORYS_FAILURE]: state =>
       produce(state, (draft: CategoryState) => {
         draft.categoryPending = false
         draft.categoryError = true
-        draft.categoryCategory = [{ posts: [''], _id: '', category: '', __v: 0 }]
+        draft.categoryCategory = [{ posts: [{}], _id: '', category: '', __v: 0 }]
       }),
 
     // 추가할 카테고리 인풋값 변경
-    [POST_ADD_CATEGORY_INPUT_CHANGE]: (state, action: Action<CategoryInputPayload>) =>
+    [POST_ADD_CATEGORY_INPUT_CHANGE]: (state, action: AddCategoryInputPayloadAction) =>
       produce(state, (draft: CategoryState) => {
         draft.addCategoryInputValue = action.payload
       }),
@@ -187,12 +217,12 @@ export default handleActions(
       }),
 
     // 변경할 카테고리 인풋값 변경
-    [PATCH_CHANGE_CATEGORY_INPUT_CHANGE]: (state, action: Action<CategoryInputPayload>) =>
+    [PATCH_CHANGE_CATEGORY_INPUT_CHANGE]: (state, action: ChangeCategoryInputPayloadAction) =>
       produce(state, (draft: CategoryState) => {
         draft.changeCategoryInputValue = action.payload
       }),
     // 변경할 카테고리 셀렉트값 변경
-    [PATCH_CHANGE_CATEGORY_CATEGORY_CHANGE]: (state, action: Action<CategoryInputPayload>) =>
+    [PATCH_CHANGE_CATEGORY_CATEGORY_CHANGE]: (state, action: ChangeCategoryCategoryPayloadAction) =>
       produce(state, (draft: CategoryState) => {
         draft.changeCategoryCategoryValue = action.payload
       }),
@@ -213,12 +243,12 @@ export default handleActions(
       }),
 
     // 삭제할 카테고리 인풋값 변경
-    [DELETE_DELETE_CATEGORY_INPUT_CHANGE]: (state, action: Action<CategoryInputPayload>) =>
+    [DELETE_DELETE_CATEGORY_INPUT_CHANGE]: (state, action: DeleteCategoryInputPayloadAction) =>
       produce(state, (draft: CategoryState) => {
         draft.deleteCategoryInputValue = action.payload
       }),
     // 삭제할 카테고리 셀렉트값 변경
-    [DELETE_DELETE_CATEGORY_CATEGORY_CHANGE]: (state, action: Action<CategoryInputPayload>) =>
+    [DELETE_DELETE_CATEGORY_CATEGORY_CHANGE]: (state, action: DeleteCategoryCategoryPayloadAction) =>
       produce(state, (draft: CategoryState) => {
         draft.deleteCategoryCategoryValue = action.payload
       }),
