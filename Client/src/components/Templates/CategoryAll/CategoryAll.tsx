016acc141cf25564809ceb3bb70a0fcc74efe823
NoMoreViolence <ljh86029926@gmail.com>
2018-05-17 22:29:55 +0900

    버튼 눌러서 포스트 더 불러오는 기능 구현

--- Client/src/components/Templates/CategoryAll/CategoryAll.tsx
+++ Client/src/components/Templates/CategoryAll/CategoryAll.tsx
@@ -21,47 +21,86 @@ interface Props {
   category: CategoryStateInside[]
 }
 
-const CategoryAll = ({ category }: Props) => {
-  const postViewer = (posts: CategoryStateInside[]) => {
-    const allPost: PostsStateInside[] = []
+class CategoryAll extends React.Component<Props> {
+  // state
+  public state = {
+    load: 10,
+    loaded: false,
+    postCount: 0
+  }
 
-    // contain all Posts
-    posts.map((object, i) => {
-      object.posts.map((object, i) => {
-        allPost.push(object)
-      })
-    })
-
-    // sort by date
-    allPost.sort(function(a, b) {
-      return a.date > b.date ? -1 : a.date < b.date ? 1 : 0
-    })
-
-    // return value
-    return allPost.map((object, i) => {
-      return (
-        <Card key={i}>
-          <CardBody>
-            <CardTitle>{object.title}</CardTitle>
-            <CardSubtitle>{object.category.category}</CardSubtitle>
-            <CardText>{object.subTitle}</CardText>
-            <NavLink to={'/' + object.category.category + '/' + object.title}>
-              <Button color="primary">See Post!</Button>
-            </NavLink>
-          </CardBody>
-        </Card>
-      )
+  // the load value, if it increase, the viewer can see many posts before
+  public handleShow = () => {
+    this.setState({
+      load: this.state.load + 10
     })
   }
-  return (
-    <Container className="category-all">
-      <Row>
-        <Col>
-          <CardColumns>{postViewer(category)}</CardColumns>
-        </Col>
-      </Row>
-    </Container>
-  )
+
+  public render() {
+    // button show or hide
+    let ShowOrHideButton = false
+    // all posts number
+    let AllPostsNum: number = 0
+    const postViewer = (posts: CategoryStateInside[]) => {
+      const allPost: PostsStateInside[] = []
+
+      // contain all Posts
+      posts.map((object, i) => {
+        object.posts.map((object, i) => {
+          AllPostsNum++
+          allPost.push(object)
+        })
+      })
+
+      // post number > load value
+      if (this.state.load < AllPostsNum) {
+        ShowOrHideButton = true
+      }
+
+      // sort by date
+      allPost.sort(function(a, b) {
+        return a.date > b.date ? -1 : a.date < b.date ? 1 : 0
+      })
+
+      // return value
+      return allPost.map((object, i) => {
+        if (i < this.state.load) {
+          return (
+            <Card key={i}>
+              <CardBody>
+                <CardTitle>{object.title}</CardTitle>
+                <CardSubtitle>{object.category.category}</CardSubtitle>
+                <CardText>{object.subTitle}</CardText>
+                <NavLink to={'/' + object.category.category + '/' + object.title}>
+                  <Button color="primary">See Post!</Button>
+                </NavLink>
+              </CardBody>
+            </Card>
+          )
+        } else {
+          return null
+        }
+      })
+    }
+    return (
+      <Container className="category-all">
+        <Row>
+          <Col>
+            <CardColumns>{postViewer(this.props.category)}</CardColumns>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            {ShowOrHideButton && (
+              <Button color="primary" size="lg" block={true} onClick={this.handleShow}>
+                More...
+              </Button>
+            )}
+          </Col>
+        </Row>
+      </Container>
+    )
+  }
 }
 
 export default CategoryAll
