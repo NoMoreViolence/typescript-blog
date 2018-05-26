9255f735df4e252000b7b13e6f55caa534e4807e
NoMoreViolence <ljh86029926@gmail.com>
2018-05-26 13:52:21 +0900

    포스트 추가 request 부분 체크 promise로 작성 완료

--- Client/src/components/Templates/Admin/PostAdd/PostAdd.tsx
+++ Client/src/components/Templates/Admin/PostAdd/PostAdd.tsx
@@ -1,25 +1,29 @@
 import * as React from 'react'
 
 import './PostAdd.css'
+import { Button, Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap'
+import { toast } from 'react-toastify'
 
 import { AddPostState } from 'store/modules/Post'
-import { CategoryState, CategoryStateInside } from 'store/modules/Category'
+import { CategoryStateInside } from 'store/modules/Category'
 
-import * as CodeMirror from 'codemirror'
-import MarkdownRenderer from 'lib/MarkDownRenderer/MarkDownRenderer'
-import { Button, Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap'
+import MarkdownEditor from 'lib/MarkDownEditor'
+import MarkdownRenderer from 'lib/MarkDownRenderer'
 
+interface AddPost {
+  category?: string
+  title?: string
+  subTitle?: string
+  mainText?: string
+}
 interface Props {
-  category: CategoryState
+  category: CategoryStateInside[]
   add: AddPostState
   changeCategory: (value: string) => any
   changeTitle: (value: string) => any
   changeSubTitle: (value: string) => any
   changeMainText: (value: string) => any
-}
-
-interface Target {
-  target: HTMLInputElement
+  addPost: (AddPost: AddPost) => any
 }
 
 interface Dropdown {
@@ -29,59 +33,104 @@ interface Dropdown {
 class PostAdd extends React.Component<Props> {
   public state = {
     leftPercentage: 0.5,
+    postAddMessage: '포스트 추가하기 !',
     showNone: false,
     dropdown: false
   }
 
-  public editor: any = null // 에디터 ref
-  public codeMirror: any = null // CodeMirror 인스턴스
-  public cursor: any = null // 에디터의 텍스트 cursor 위치
-  public initializeEditor = async () => {
-    await this.editor
-    this.codeMirror = CodeMirror(this.editor, {
-      mode: 'markdown',
-      theme: 'monokai',
-      lineNumbers: true, // 좌측에 라인넘버 띄우기
-      lineWrapping: true // 내용이 너무 길면 다음 줄에 작성
-    })
-    this.codeMirror.on('change', this.handleChangeMarkdown)
-  }
-  // change Mark Down Method
-  public handleChangeMarkdown = async (doc: any) => {
-    const { changeMainText } = this.props
-    this.cursor = doc.getCursor() // 텍스트 cursor 의 위치를 저장합니다
-    if (this.props.add.mainText !== (await doc.getValue())) {
-      changeMainText(await doc.getValue())
+  // button click
+  public handleShowNone = async () => {
+    if (this.state.showNone === false) {
+      this.setState({
+        showNone: !this.state.showNone,
+        leftPercentage: 0.5,
+        postAddMessage: '포스트 추가 접기 !'
+      })
+    } else {
+      this.setState({
+        showNone: !this.state.showNone,
+        postAddMessage: '포스트 추가하기 !'
+      })
     }
-  }
-  //
-  //
-  //
-  public componentDidMount() {
-    this.initializeEditor()
   }
 
-  public componentDidUpdate(prevProps: Props) {
-    // markdown이 변경되면 에디터의 값도 변경해줍니다.
-    // 이 과정에서 텍스트 커서의 위치가 초기화 되기 때문에,
-    // 저장해둔 커서의 위치가 있으면 해당 위치로 설정합니다.
-    if (prevProps.add.mainText !== this.props.add.mainText) {
-      const { codeMirror, cursor } = this
-      if (!codeMirror) {
-        return
-      } // 인스턴스가 아직 안만들어진 경우
-      codeMirror.setValue(this.props.add.mainText)
-      if (!cursor) {
-        return
-      } // 커서가 없는 경우
-      codeMirror.setCursor(cursor)
-    }
+  // Dropdown Toogle
+  public handleToogle = () => {
+    this.setState({
+      dropdown: !this.state.dropdown
+    })
   }
-  //
-  //
-  //
-  //
-  //
+  // Category Select Change
+  public handleSelect = (e: Dropdown) => {
+    this.props.changeCategory(e.currentTarget.textContent)
+  }
+
+  // Submit => Post Add
+  public handleSubmit = () => {
+    const categoryCheck = (post: AddPost) => {
+      if (post.category !== '카테고리 선택') {
+        return new Promise(function(resolve, reject) {
+          resolve(post)
+        })
+      }
+      return new Promise(function(resolve, reject) {
+        reject(new Error('카테고리를 선택해 주세요 !'))
+      })
+    }
+
+    const titleCheck = (post: AddPost) => {
+      if (post.title !== '') {
+        return new Promise(function(resolve, reject) {
+          resolve(post)
+        })
+      }
+      return new Promise(function(resolve, reject) {
+        reject(new Error('포스트 제목을 입력해 주세요 !'))
+      })
+    }
+
+    const subTitleCheck = (post: AddPost) => {
+      if (post.subTitle !== '') {
+        return new Promise(function(resolve, reject) {
+          resolve(post)
+        })
+      }
+      return new Promise(function(resolve, reject) {
+        reject(new Error('포스트 부제목을 입력해 주세요 !'))
+      })
+    }
+
+    const mainTextCheck = (post: AddPost) => {
+      if (post.mainText !== '') {
+        return new Promise(function(resolve, reject) {
+          resolve(post)
+        })
+      }
+      return new Promise(function(resolve, reject) {
+        reject(new Error('포스트 내용을 입력해 주세요 !'))
+      })
+    }
+
+    const requestToServer = (post: AddPost) => {
+      toast('포스트 추가가 완료되었습니다 !')
+    }
+
+    const onError = (err: Error) => {
+      toast(err.message)
+    }
+    categoryCheck({
+      category: this.props.add.category,
+      title: this.props.add.title,
+      subTitle: this.props.add.subTitle,
+      mainText: this.props.add.mainText
+    })
+      .then(titleCheck)
+      .then(subTitleCheck)
+      .then(mainTextCheck)
+      .then(requestToServer)
+      .catch(onError)
+  }
+
   // separator click, and mouse move
   public handleMouseMove = (e: MouseEvent) => {
     this.setState({
@@ -98,56 +147,15 @@ class PostAdd extends React.Component<Props> {
     document.body.addEventListener('mousemove', this.handleMouseMove)
     window.addEventListener('mouseup', this.handleMouseUp)
   }
-  //
-  //
-  //
-  //
-  //
-
-  // title & subTitle change
-  public handleChange = (e: Target) => {
-    if (e.target.name === 'title') {
-      this.props.changeTitle(e.target.value)
-    } else if (e.target.name === 'sub-title') {
-      this.props.changeSubTitle(e.target.value)
-    }
-  }
-
-  // button click
-  public handleShowNone = async () => {
-    this.setState({
-      showNone: !this.state.showNone
-    })
-    // the setState method is call lifecycle method.
-    // therefore, Although I did setState, the showNone value is false
-    if (this.state.showNone === false) {
-      await this.initializeEditor()
-      await this.codeMirror
-      this.codeMirror.setValue(this.props.add.mainText)
-    }
-  }
-
-  // dropdown toogle
-  public handleToogle = () => {
-    this.setState({
-      dropdown: !this.state.dropdown
-    })
-  }
-  // category Change
-  public handleSelect = (e: Dropdown) => {
-    this.props.changeCategory(e.currentTarget.textContent)
-  }
 
   public render() {
     const { leftPercentage } = this.state
-    // flex value
     const leftStyle = {
       flex: leftPercentage
     }
     const rightStyle = {
       flex: 1 - leftPercentage
     }
-    // separator state config
     const separatorStyle = {
       left: `${leftPercentage * 100}%`
     }
@@ -166,47 +174,40 @@ class PostAdd extends React.Component<Props> {
     return (
       <div className="add-editor-template">
         <Button block={true} color="primary" onClick={this.handleShowNone}>
-          포스트 추가하기 !
+          {this.state.postAddMessage}
         </Button>
         {this.state.showNone && (
           <React.Fragment>
             <div className="add-editor-select-category">
-              <Dropdown
-                className="add-editor-dropdown-button"
-                isOpen={this.state.dropdown}
-                toggle={this.handleToogle}
-                size="lg"
-              >
+              <Dropdown className="add-editor-button" isOpen={this.state.dropdown} toggle={this.handleToogle} size="lg">
                 <DropdownToggle outline={true} color="primary" caret={true}>
-                  {this.props.category.post.addCategory}
+                  {this.props.add.category}
                 </DropdownToggle>
                 <DropdownMenu>
                   <DropdownItem onClick={this.handleSelect}>카테고리 선택</DropdownItem>
-                  {CurrentCategoryChangeBar(this.props.category.categoryCategory)}
+                  {CurrentCategoryChangeBar(this.props.category)}
                 </DropdownMenu>
               </Dropdown>
+
+              <div className="add-editor-button">
+                <Button onClick={this.handleSubmit} color="primary" size="lg">
+                  포스트 생성 하기 !
+                </Button>
+              </div>
             </div>
             <div className="add-editor-and-viewer">
               <div className="add-editor" style={leftStyle}>
-                {}
                 <div className="add-editor-inside">
-                  <input
-                    className="add-editor-title"
-                    placeholder="제목을 입력하세요"
-                    name="title"
-                    value={this.props.add.title}
-                    onChange={this.handleChange}
+                  <MarkdownEditor
+                    title={this.props.add.title}
+                    changeTitle={this.props.changeTitle}
+                    changeSubTitle={this.props.changeSubTitle}
+                    subTitle={this.props.add.subTitle}
+                    MainText={this.props.add.mainText}
+                    changeMainText={this.props.changeMainText}
+                    state={this.state.showNone}
                   />
-                  <input
-                    className="add-editor-sub-title"
-                    placeholder="부제목을 입력하세요"
-                    name="sub-title"
-                    value={this.props.add.subTitle}
-                    onChange={this.handleChange}
-                  />
-                  <div className="code-editor" ref={ref => (this.editor = ref)} />
                 </div>
-                {}
               </div>
               <div className="add-preview" style={rightStyle}>
                 <div className="add-preview-inside">
