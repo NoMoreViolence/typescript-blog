4e21f2692c53a7a43a4802b2e867050cd9013a7d
NoMoreViolence <ljh86029926@gmail.com>
2018-05-27 00:39:08 +0900

    Post Container 부분 추가 & 마크다운 편집기 색상 코드 추가

--- Client/src/components/Pages/Admin/Admin.tsx
+++ Client/src/components/Pages/Admin/Admin.tsx
@@ -2,10 +2,27 @@ import * as React from 'react'
 import './Admin.css'
 import { Container, Row, Col } from 'reactstrap'
 
+// category
 import CurrentCategoryContainer from 'containers/Admin/CurrentCategoryContainer'
 import CategoryAddContainer from 'containers/Admin/CategoryAddContainer'
 import CategoryChangeContainer from 'containers/Admin/CategoryChangeContainer'
 import CategoryDeleteContainer from 'containers/Admin/CategoryDeleteContainer'
+// post
+import PostAddContainer from 'containers/Admin/PostAddContainer'
+import PostChangeContainer from 'containers/Admin/PostChangeContainer'
+import PostDeleteContainer from 'containers/Admin/PostDeleteContainer'
+
+import 'codemirror/mode/markdown/markdown' // 마크다운 문법 색상
+// 마크다운 내부에 들어가는 코드 색상
+import 'codemirror/mode/javascript/javascript'
+import 'codemirror/mode/htmlmixed/htmlmixed'
+import 'codemirror/mode/jsx/jsx'
+import 'codemirror/mode/css/css'
+import 'codemirror/mode/shell/shell'
+// CodeMirror 를 위한 CSS 스타일
+import 'codemirror/lib/codemirror.css'
+import 'codemirror/theme/monokai.css'
+import 'codemirror/theme/material.css'
 
 const Admin: React.SFC = () => (
   <Container>
@@ -38,6 +55,21 @@ const Admin: React.SFC = () => (
       <Row>
         <Col>
           <h1 className="post-container">Post</h1>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <PostAddContainer />
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <PostChangeContainer />
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <PostDeleteContainer />
         </Col>
       </Row>
     </Container>
