6ef21068d75e45a37ee6acdcc65f3173f9482230
NoMoreViolence <ljh86029926@gmail.com>
2018-05-31 00:09:09 +0900

    프로그래스 바 완성

--- Client/src/lib/ProgressBar/ProgressBar.tsx
+++ Client/src/lib/ProgressBar/ProgressBar.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react'
+
+import './ProgressBar.css'
+
 import { CategoryState } from 'store/modules/Category'
 import { LoadPostState, AddPostState, ChangePostState, DeletePostState } from 'store/modules/Post'
 
 import { connect } from 'react-redux'
 import { StoreState } from 'store/modules'
-
-import * as Progressbar from 'air-progressbar'
 
 interface Props {
   category: CategoryState
@@ -15,21 +16,62 @@ interface Props {
   deletePost: DeletePostState
 }
 
+interface ProgressBarMainProps {
+  loadCategoryPending?: boolean
+  addCategoryPending?: boolean
+  changeCategoryPending?: boolean
+  deleteCategoryPending?: boolean
+  loadPostPending?: boolean
+  addPostPending?: boolean
+  changePostPending?: boolean
+  deletePostPending?: boolean
+}
+
 // real
-class ProgressBar extends React.Component<Props> {
+class ProgressBar extends React.Component<ProgressBarMainProps> {
   public state = { loading: 0 }
-  public componentDidUpdate(PrevProps: Props) {
-    // awiejfo;eawfj
+  public componentDidUpdate(prevProps: ProgressBarMainProps) {
+    const {
+      loadCategoryPending,
+      addCategoryPending,
+      changeCategoryPending,
+      deleteCategoryPending,
+      loadPostPending,
+      addPostPending,
+      changePostPending,
+      deletePostPending
+    } = this.props
+
+    if (this.props !== prevProps) {
+      if (
+        loadCategoryPending === true ||
+        addCategoryPending === true ||
+        changeCategoryPending === true ||
+        deleteCategoryPending === true ||
+        loadPostPending === true ||
+        addPostPending === true ||
+        changePostPending === true ||
+        deletePostPending === true
+      ) {
+        this.setState({
+          loading: 40
+        })
+      } else {
+        this.setState({ loading: 100 })
+      }
+    }
+    return true
   }
   public render() {
+    const style = {
+      width: `${this.state.loading}%`
+    }
     return (
-      <Progressbar.Line
-        percent={this.state.loading}
-        strokeWidth={0.5}
-        strokeColor="#2DB7F5"
-        tailColor="gray"
-        strokeLinecap="round"
-      />
+      <div className="progress-container">
+        <div className="progress-bar-custom">
+          <div className="progress-loading-bar" style={style} />
+        </div>
+      </div>
     )
   }
 }
@@ -37,11 +79,14 @@ class ProgressBar extends React.Component<Props> {
 // container
 const ProgressBarContainer: React.SFC<Props> = Props => (
   <ProgressBar
-    category={Props.category}
-    loadPost={Props.loadPost}
-    addPost={Props.addPost}
-    changePost={Props.changePost}
-    deletePost={Props.deletePost}
+    loadCategoryPending={Props.category.categoryPending}
+    addCategoryPending={Props.category.addCategoryPending}
+    changeCategoryPending={Props.category.changeCategoryPending}
+    deleteCategoryPending={Props.category.deleteCategoryPending}
+    loadPostPending={Props.loadPost.pending}
+    addPostPending={Props.addPost.pending}
+    changePostPending={Props.changePost.pending}
+    deletePostPending={Props.deletePost.pending}
   />
 )
 
