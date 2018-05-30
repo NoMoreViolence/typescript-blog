8e6a6f1d08ff0560c2516d6f8c5ff06c9f72dd27
NoMoreViolence <ljh86029926@gmail.com>
2018-05-31 00:41:39 +0900

    separate html and css

--- Client/src/components/Pages/Admin/Admin.tsx
+++ Client/src/components/Pages/Admin/Admin.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react'
+
 import './Admin.css'
-// 공통 에디터 전용 css 적용
-import 'components/commonCSS/editor.css'
 
 // category
 import CurrentCategoryContainer from 'containers/Admin/CurrentCategoryContainer'
@@ -13,33 +12,18 @@ import PostAddContainer from 'containers/Admin/PostAddContainer'
 import PostChangeContainer from 'containers/Admin/PostChangeContainer'
 import PostDeleteContainer from 'containers/Admin/PostDeleteContainer'
 
-import 'codemirror/mode/markdown/markdown' // 마크다운 문법 색상
-// 마크다운 내부에 들어가는 코드 색상
-import 'codemirror/mode/javascript/javascript'
-import 'codemirror/mode/htmlmixed/htmlmixed'
-import 'codemirror/mode/jsx/jsx'
-import 'codemirror/mode/css/css'
-import 'codemirror/mode/shell/shell'
-// CodeMirror 를 위한 CSS 스타일
-import 'codemirror/lib/codemirror.css'
-import 'codemirror/theme/monokai.css'
-import 'codemirror/theme/material.css'
-
 const Admin: React.SFC = () => {
-  const styled = {
-    marginTop: '5%'
-  }
   return (
     <React.Fragment>
       <div className="layout-container">
-        <h1 style={styled}>Category</h1>
+        <h1 className="admin-logo">Category</h1>
         <CurrentCategoryContainer />
         <CategoryAddContainer />
         <CategoryChangeContainer />
         <CategoryDeleteContainer />
       </div>
       <div className="layout-container">
-        <h1 style={styled}>Post</h1>
+        <h1 className="admin-logo">Post</h1>
       </div>
       <PostAddContainer />
       <PostChangeContainer />
