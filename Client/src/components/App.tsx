0bddae38fadd74f3a210b257bc08e82c4df2fb8f
NoMoreViolence <ljh86029926@gmail.com>
2018-05-31 00:41:53 +0900

    clean app.js

--- Client/src/components/App.tsx
+++ Client/src/components/App.tsx
@@ -2,35 +2,42 @@ import * as React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
-// css 모듈
+// css module
 import 'components/commonCSS/layout.css'
 import 'components/commonCSS/cardView.css'
 import 'components/commonCSS/button.css'
 
-// 페이지들
+// Pages
 import Basic from 'components/Pages/Basic'
 import Login from 'components/Pages/Login'
 import Header from 'components/Pages/Header'
 import CategoryAll from 'components/Pages/CategoryAll'
 import CategorySelect from 'components/Pages/CategorySelect'
 import Admin from 'components/Pages/Admin'
+import PostSelect from 'components/Pages/PostSelect'
+import NotFound from 'components/Pages/NotFound'
+
+import ProgressBar from 'lib/ProgressBar'
 
 class App extends React.Component {
   public render() {
     return (
       <React.Fragment>
-        <Basic />
-        <ToastContainer />
-        <Header />
+        <ProgressBar />
+        <div className="main">
+          <Basic />
+          <ToastContainer />
+          <Header />
 
-        <Switch>
-          <Route exact={true} path="/admin/login" component={Login} />
-          <Route exact={true} path="/admin" component={Admin} />
-        </Switch>
-
-        <Route exact={true} path="/" component={CategoryAll} />
-        {/* url="/" 부터 url="/:category" 까지, 메인이나 특정 카테고리에 따른 포스트들을 보여줌 */}
-        <Route exact={true} path="/:category" component={CategorySelect} />
+          <Switch>
+            <Route exact={true} path="/admin/login" component={Login} />
+            <Route exact={true} path="/admin" component={Admin} />
+            <Route exact={true} path="/" component={CategoryAll} />
+            <Route exact={true} path="/:category" component={CategorySelect} />
+            <Route exact={true} path="/:category/:post" component={PostSelect} />
+            <Route path="/" component={NotFound} />
+          </Switch>
+        </div>
       </React.Fragment>
     )
   }
