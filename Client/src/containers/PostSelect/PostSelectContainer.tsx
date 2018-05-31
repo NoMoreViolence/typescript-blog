9f246fc1d4a4657ffdd4969e6a897347a543c722
NoMoreViolence <ljh86029926@gmail.com>
2018-05-31 23:25:28 +0900

    container에 load State 추가해서 원형 로딩 바 구현

--- Client/src/containers/PostSelect/PostSelectContainer.tsx
+++ Client/src/containers/PostSelect/PostSelectContainer.tsx
@@ -1,19 +1,21 @@
 import * as React from 'react'
 import PostSelect from 'components/Templates/PostSelect'
 
-import { ShowPostState, PostActions } from 'store/modules/Post'
+import { ShowPostState, PostActions, LoadPostState } from 'store/modules/Post'
 
 import { connect } from 'react-redux'
 import { StoreState } from 'store/modules'
 import { bindActionCreators } from 'redux'
 
 interface Props {
+  load: LoadPostState
   show: ShowPostState
   PostActions: typeof PostActions
 }
 
 const PostSelectContainer: React.SFC<Props> = Props => (
   <PostSelect
+    loading={Props.load.pending}
     category={Props.show.category}
     title={Props.show.title}
     subTitle={Props.show.subTitle}
@@ -25,7 +27,8 @@ const PostSelectContainer: React.SFC<Props> = Props => (
 
 export default connect(
   ({ Post }: StoreState) => ({
-    show: Post.show
+    show: Post.show,
+    load: Post.load
   }),
   dispatch => ({ PostActions: bindActionCreators(PostActions, dispatch) })
 )(PostSelectContainer)
