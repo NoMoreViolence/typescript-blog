eede6188ff7eade9f5fa9bbef8e758466b46beef
NoMoreViolence <ljh86029926@gmail.com>
2018-06-03 10:32:09 +0900

    카테고리 추가 submit method promise 처리 & 엄격한 오류 검사 완료

--- Client/src/components/Templates/Admin/CategoryAdd/CategoryAdd.tsx
+++ Client/src/components/Templates/Admin/CategoryAdd/CategoryAdd.tsx
@@ -15,108 +15,167 @@ interface Props {
   categoryDone: () => void
 }
 
+interface PostAddMethodInterface {
+  loginLogined: boolean
+  addCategoryInputValue: string
+}
+
 interface Target {
   target: HTMLInputElement
 }
 
-const CategoryAdd = withRouter<Props & RouteComponentProps<any>>(
-  class CategoryAdd extends React.Component<Props & RouteComponentProps<any>> {
-    // 인풋 focus 사용 위해
-    public addCategoryInput: any
+class CategoryAdd extends React.Component<Props & RouteComponentProps<any>> {
+  // to use focus()
+  public addCategoryInput: any
 
-    // 입력 체인지
-    public handleChange = (e: Target) => {
-      this.props.addCategoryInputChange(e.target.value)
+  // change category input value
+  public handleChange = (e: Target) => {
+    this.props.addCategoryInputChange(e.target.value)
+  }
+
+  // category add method
+  public handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    // stop basic stuff
+    e.preventDefault()
+    // the value that can find out where error started
+    let count = 0
+
+    const {
+      loginLogined,
+      addCategoryInputValue,
+      addCategoryInputChange,
+      addCategory,
+      categoryLoad,
+      logout,
+      categoryDone,
+      history
+    } = this.props
+
+    // check user is logined or not
+    const userAdminCheck = (data: PostAddMethodInterface) => {
+      count++
+      // compare logined or not
+      if (data.loginLogined) {
+        return new Promise(function(resolve, reject) {
+          resolve(data)
+        })
+      }
+      return new Promise(function(resolve, reject) {
+        reject(new Error('인증된 사용자가 아닙니다 !'))
+      })
     }
 
-    // 카테고리 추가 메소드
-    public handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-      e.preventDefault()
+    // check the input value is '' or not
+    const inputValueCheck = (data: PostAddMethodInterface) => {
+      count++
+      // compare the value exist or not
+      if (data.addCategoryInputValue.trim() !== '') {
+        return new Promise(function(resolve, reject) {
+          resolve(data)
+        })
+      }
+      return new Promise(function(resolve, reject) {
+        reject(new Error('추가할 카테고리를 입력해 주세요 !'))
+      })
+    }
 
-      const {
-        loginLogined,
-        addCategoryInputValue,
-        addCategoryInputChange,
-        addCategory,
-        categoryLoad,
-        logout,
-        categoryDone,
-        history
-      } = this.props
+    // check the input value.toUpperCase() is 'admin' or not
+    const inputValueAdminCheck = (data: PostAddMethodInterface) => {
+      count++
+      // compare the value is 'admin' or not
+      if (data.addCategoryInputValue.toLowerCase().trim() !== 'admin') {
+        return new Promise(function(resolve, reject) {
+          resolve(data)
+        })
+      }
+      return new Promise(function(resolve, reject) {
+        reject(new Error(`'${data}' 이름의 카테고리는 생성 불가능 합니다 !`))
+      })
+    }
 
-      // 카테고리 입력값이 없거나 관리자 권한이 아닐 경우
-      if (loginLogined !== false && addCategoryInputValue !== '') {
-        if (addCategoryInputValue.toLowerCase() !== 'admin') {
-          addCategory(addCategoryInputValue)
-            .then((res: any) => {
-              // 카테고리 추가 성공
-              toast(res.value.data.message)
-              // 카테고리 Input Select 초기화 & 리로딩
-              categoryDone()
-              categoryLoad()
-            })
-            .catch((err: any) => {
-              // tslint:disable-next-line:no-console
-              console.log(err.response)
-              // 사용자의 해킹 시도
-              if (err.response.data.type === 'undefinded token' || err.response.data.type === 'invalid token') {
-                toast('인증된 사용자가 아닙니다 !')
-                // 로그아웃 메소드로 loginLogined false & 세션 스토리지 초기화 & 홈페이지로 이동
-                logout()
-                sessionStorage.clear()
-                history.push('/')
-              }
-              // 사용자의 시도 실패
-              else if (err.response.data.type === 'server error') {
-                toast('카테고리가 중복되었습니다 !')
-                this.addCategoryInput.focus()
-                addCategoryInputChange('')
-              } else {
-                toast('알 수 없는 에러입니다 !')
-              }
-            })
-        } else {
-          toast('관리자 페이지 url 로 카테고리 등록은 불가능 합니다 !')
-        }
-      } else {
-        // 인가된 사용자가 아닐 경우
-        if (loginLogined === false) {
-          toast('관리자만 접근 / 엑세스 가능합니다 !')
-          // 로그아웃 메소드로 loginLogined false & 세션 스토리지 초기화 & 홈페이지로 이동
-          logout()
-          sessionStorage.clear()
-          history.push('/')
-        }
-        // 아무 글자도 입력하지 않은 경우
-        else if (addCategoryInputValue === '') {
-          toast('글자를 입력해 주세요 !')
+    // create category
+    const requestToServer = (data: PostAddMethodInterface) => {
+      // request to server for create category
+      addCategory(data.addCategoryInputValue)
+        // succeed create category
+        .then((res: { value: any; action: any }) => {
+          toast(res.action.payload.data.message)
+          categoryDone()
+          categoryLoad()
+        })
+        // failure create category
+        .catch((err: { response: any }) => {
+          addCategoryInputChange('')
           this.addCategoryInput.focus()
-        }
+          toast(err.response.data.message)
+
+          if (err.response.data.type) {
+            toast('서비스를 이용하시려면 다시 로그인 해 주세요 !')
+            logout()
+            history.push('/')
+          }
+        })
+    }
+
+    // error handler
+    const onError = (err: Error) => {
+      switch (count) {
+        // not admin
+        case 1:
+          toast('관리자만 이용 가능합니다 !')
+          addCategoryInputChange('')
+          logout()
+          history.push('/')
+          count = 0
+          break
+        // no input value
+        case 2:
+          toast('추가할 카테고리를 입력해 주세요 !')
+          addCategoryInputChange('')
+          this.addCategoryInput.focus()
+          count = 0
+          break
+        // input value.upUpperCase is 'admin'
+        case 3:
+          toast(`관리자 url의 카테고리는 생성될 수 없습니다 !`)
+          addCategoryInputChange('')
+          this.addCategoryInput.focus()
+          count = 0
+          break
+        default:
+          break
       }
     }
 
-    public render() {
-      return (
-        <div className="category-add-container">
-          <p className="category-add-p">카테고리 추가</p>
-          <Form onSubmit={this.handleSubmit}>
-            <InputGroup>
-              <Input
-                name="addCategory"
-                placeholder="추가할 카테고리 입력"
-                value={this.props.addCategoryInputValue}
-                onChange={this.handleChange}
-                innerRef={innerRef => (this.addCategoryInput = innerRef)}
-              />
-              <Button outline={true} color="primary">
-                카테고리 추가
-              </Button>
-            </InputGroup>
-          </Form>
-        </div>
-      )
-    }
+    // Promise
+    userAdminCheck({ loginLogined, addCategoryInputValue })
+      .then(inputValueCheck)
+      .then(inputValueAdminCheck)
+      .then(requestToServer)
+      .catch(onError)
   }
-)
 
-export default CategoryAdd
+  public render() {
+    return (
+      <div className="category-add-container">
+        <p className="category-add-p">카테고리 추가</p>
+        <Form onSubmit={this.handleSubmit}>
+          <InputGroup>
+            <Input
+              name="addCategory"
+              placeholder="추가할 카테고리 입력"
+              value={this.props.addCategoryInputValue}
+              onChange={this.handleChange}
+              innerRef={innerRef => (this.addCategoryInput = innerRef)}
+            />
+            <Button outline={true} color="primary">
+              카테고리 추가
+            </Button>
+          </InputGroup>
+        </Form>
+      </div>
+    )
+  }
+}
+
+export default withRouter(CategoryAdd)
