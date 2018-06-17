088349ff20c878d514b82f5255d56e13ee5859c6
NoMoreViolence <ljh86029926@gmail.com>
2018-06-18 08:38:32 +0900

    필요없는 함수 제거

--- server/routes/api/category/category.controller.js
+++ server/routes/api/category/category.controller.js
@@ -12,35 +12,48 @@ exports.showTitleAndSubTitle = (req, res) => {
   // category Check, if data is 'admin' throw error, because 'admin' is can't be a category Name
   const categoryNameAdminCheck = data => {
     // right value
-    if (data.toLowerCase() !== 'admin') {
+    if (data.requestCategory.toLowerCase() !== 'admin') {
       return Promise.resolve(data)
     }
     // 'admin' throw error
-    return Promise.reject(new Error(`'${data}' 는 관리자 이름의 카테고리라 존재할 수 없습니다 !`))
+    return Promise.reject(new Error(`'${data.requestCategory}' 는 관리자 이름의 카테고리라 존재할 수 없습니다 !`))
   }
 
   // find Category
-  const bringCategoryData = data => {
-    if (data === 'categories') return Category.findAllCategoriesTitleAndSubTitle()
-    return Category.findSomeCategorysTitleAndSubTitle(data)
+  const bringCategoryData = async data => {
+    // if the request is all category
+    if (data.requestCategory === 'categories') {
+      const CategoryData = await Category.findAllCategoriesTitleAndSubTitle()
+      return Promise.resolve({ ...data, category: CategoryData, type: 'ALL' })
+    }
+
+    // if the request is select category
+    const CategoryData = await Category.findSomeCategorysTitleAndSubTitle(data.requestCategory)
+    return Promise.resolve({ ...data, category: CategoryData, type: 'Select' })
   }
 
   // check the category data is null or not
   const checkBringedCategoryData = data => {
-    // if the data is null throw error
-    if (data.length !== 0) {
+    // select
+    if (data.type === 'Select' && data.category.length !== 0) {
       return Promise.resolve(data)
     }
+
+    // all
+    if (data.type === 'ALL') {
+      return Promise.resolve(data)
+    }
+
     return Promise.reject(new Error('검색하신 카테고리가 존재하지 않습니다 !'))
   }
 
   // All category Search data === data
-  const responseToServer = data => {
+  const respondToClient = data => {
     // respond to client with all category and all category data ( title, subTitle )
     res.json({
       success: true,
-      message: `'${category}' 카테고리 정보 가져오기 성공 !`,
-      value: data
+      message: `'${data.requestCategory}' 카테고리 정보 가져오기 성공 !`,
+      value: data.category
     })
   }
 
@@ -54,10 +67,10 @@ exports.showTitleAndSubTitle = (req, res) => {
   }
 
   // Promise
-  categoryNameAdminCheck(category.trim())
+  categoryNameAdminCheck({ requestCategory: category.trim(), category: null, type: null })
     .then(bringCategoryData)
     .then(checkBringedCategoryData)
-    .then(responseToServer)
+    .then(respondToClient)
     .catch(onError)
 }
 
@@ -71,53 +84,45 @@ exports.showTitleAndSubTitle = (req, res) => {
       'x-access-token':  sessionStorage.getItem('token')
     }
 */
-// Create category
+// Category create
 exports.categoryCreate = (req, res) => {
   const { category } = req.params
 
-  // category text null check
-  const categoryNullCheck = data => {
-    if (data !== '') {
-      return Promise.resolve(data)
-    }
-    return Promise.reject(new Error('추가할 카테고리 값이 없습니다 !'))
-  }
-
-  // category name admin check
+  // Category name admin check
   const categoryNameAdminCheck = data => {
-    // right value
-    if (data.toLowerCase() !== 'admin') {
+    // Right value
+    if (data.category.toLowerCase() !== 'admin') {
       return Promise.resolve(data)
     }
     // 'admin' throw error
-    return Promise.reject(new Error(`'${data}' 이름의 카테고리는 생성이 불가능 합니다 !`))
+    return Promise.reject(new Error(`'${data.category}' 이름의 카테고리는 생성이 불가능 합니다 !`))
   }
 
-  // category same name check
+  // Category same name check
   const categoryNameSameCheck = async data => {
-    // if same category name exists, throw error
-    if ((await Category.findSameCategoryRegex(data)) === null) {
+    // If same category name exists, throw error
+    if ((await Category.findSameCategoryRegex(data.category)) === null) {
       return Promise.resolve(data)
     }
-    return Promise.reject(new Error(`'${data}' 이름의 카테고리가 이미 존재합니다 !`))
+    return Promise.reject(new Error(`'${data.category}' 이름의 카테고리가 이미 존재합니다 !`))
   }
 
-  // create category
+  // Create category
   const categoryCreate = async data => {
-    await Category.createCategory(data)
+    await Category.createCategory(data.category)
     return Promise.resolve(data)
   }
 
-  // respondToServer
-  const respondToServer = data => {
+  // RespondToClient
+  const respondToClient = data => {
     res.json({
       success: true,
-      message: `'${data}' 카테고리 생성 완료 !`,
-      value: data
+      message: `'${data.category}' 카테고리 생성 완료 !`,
+      value: data.category
     })
   }
 
-  // respond error with err.message & none value
+  // Respond error with err.message & none value
   const onError = err => {
     res.status(409).json({
       success: false,
@@ -127,11 +132,10 @@ exports.categoryCreate = (req, res) => {
   }
 
   // Promise
-  categoryNullCheck(category.trim())
-    .then(categoryNameAdminCheck)
+  categoryNameAdminCheck({ category: category.trim() })
     .then(categoryNameSameCheck)
     .then(categoryCreate)
-    .then(respondToServer)
+    .then(respondToClient)
     .catch(onError)
 }
 
@@ -209,7 +213,7 @@ exports.categoryChange = (req, res) => {
   }
 
   // response
-  const respondToServer = data => {
+  const respondToClient = data => {
     res.json({
       success: true,
       message: `'${data.category}' 가 '${data.changeCategory}' 로 변경이 완료됨 !`,
@@ -233,7 +237,7 @@ exports.categoryChange = (req, res) => {
     .then(bothCategorySameCheck)
     .then(newCategoryExistCheck)
     .then(categoryChange)
-    .then(respondToServer)
+    .then(respondToClient)
     .catch(onError)
 }
 
@@ -277,10 +281,10 @@ exports.categoryDelete = (req, res) => {
   }
 
   // response
-  const respondToServer = data => {
+  const respondToClient = data => {
     res.json({
       success: true,
-      message: `'${data}' 카테고리가 삭제 되었습니다 !`,
+      message: `'${data.category}' 카테고리가 삭제 되었습니다 !`,
       value: [data.category]
     })
   }
@@ -298,6 +302,6 @@ exports.categoryDelete = (req, res) => {
   categoryExistCheck({ category: category.trim(), doubleCheck: doubleCheck.trim() })
     .then(categoryDoubleCheck)
     .then(categoryDelete)
-    .then(respondToServer)
+    .then(respondToClient)
     .catch(onError)
 }
