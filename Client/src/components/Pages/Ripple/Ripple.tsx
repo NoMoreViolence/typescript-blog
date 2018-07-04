b883aed47f8f58c67de2c43f03e8115c4e24b474
NoMoreViolence <ljh86029926@gmail.com>
2018-07-05 00:52:01 +0900

    댓글 페이지

---
+++ Client/src/components/Pages/Ripple/Ripple.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+import RippleContainer from 'containers/Ripple/RippleContainer'
+
+import './Ripple.css'
+
+const Ripple: React.SFC = () => (
+  <div className="ripple-container">
+    <RippleContainer />
+  </div>
+)
+export default Ripple
