fc35cfbd4cd31f4d1d486e174fbbc41f87813691
NoMoreViolence <ljh86029926@gmail.com>
2018-07-24 00:04:16 +0900

    플러그인 추가, vender와 app 의 중복되는 코드 제거

--- blog-frontend/config/webpack.config.prod.js
+++ blog-frontend/config/webpack.config.prod.js
@@ -235,6 +235,10 @@ module.exports = {
     ]
   },
   plugins: [
+    // CommonsChunkPlugin 도 적용하세요.
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor'
+    }),
     // Makes some environment variables available in index.html.
     // The public URL is available as %PUBLIC_URL% in index.html, e.g.:
     // <link rel="shortcut icon" href="%PUBLIC_URL%/favicon.ico">
