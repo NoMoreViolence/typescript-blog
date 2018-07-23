ee650cd7d1e87e0f5f7c8e5dee764861e3a2f690
NoMoreViolence <ljh86029926@gmail.com>
2018-07-23 22:42:30 +0900

    testuser

--- blog-backend/config.js
+++ blog-backend/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  secret: "YesHelloBitches",
-  mongodbUri: "mongodb://testuser:<>@ds143451.mlab.com:43451/db-test-server"
-};
+  secret: 'YesHelloBitches',
+  mongodbUri: 'mongodb://testuser:050607jihoon@ds143451.mlab.com:43451/db-test-server'
+}
