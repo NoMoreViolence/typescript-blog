1067aaf0a13709b3f8ca75ebcf4749fc134d927c
NoMoreViolence <ljh86029926@gmail.com>
2019-04-27 22:32:07 +0900

    Backend - Token middleware

--- blog-backend/src/middlewares/token.middleware.ts
+++ blog-backend/src/middlewares/token.middleware.ts
@@ -1,5 +1,6 @@
 import { authorize$ as jwt$, VerifyOptions } from '@marblejs/middleware-jwt';
+import { of } from 'rxjs';
 
-const jwtConfig: VerifyOptions = { secret: process.env.KEY_OF_FUCKING_SECRET };
+const jwtConfig: VerifyOptions = { algorithms: ['HS512'], secret: process.env.KEY_OF_FUCKING_SECRET };
 
-// export const authorize$ = jwt$(jwtConfig);
+export const authorize$ = jwt$(jwtConfig, payload => of(payload));
